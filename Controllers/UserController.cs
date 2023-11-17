523b603b993faddfef7b1d78d3425c2043300b51
Андрей Баран <andreybaran23@gmail.com>
2023-11-17 12:46:19 +0200

    Completed solution architecture, also added user entity, repository, dbContext, and tested it

---
+++ Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using LabWebAPI.Contracts.Data;
+using LabWebAPI.Contracts.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabWebApi.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private IRepository<LabWebAPI.Contracts.Data.Entities.User> _userRepository;
+        public UserController(IRepository<LabWebAPI.Contracts.Data.Entities.User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddUser(LabWebAPI.Contracts.Data.Entities.User user)
+        {
+            await _userRepository.AddAsync(user);
+            await _userRepository.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
