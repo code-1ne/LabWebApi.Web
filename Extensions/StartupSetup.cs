b645345382bd3f586bc44451cb97e40d7e2d20f7
Андрей Баран <andreybaran23@gmail.com>
2023-11-18 17:27:48 +0200

    laba3 3 changes

---
+++ Extensions/StartupSetup.cs
@@ -0,0 +1,65 @@
+﻿using LabWebAPI.Contracts.Data.Entities;
+using LabWebAPI.Database.Data;
+using LabWebAPI.Database.Data.Seeding;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Models;
+
+namespace LabWebApi.Web.Extensions
+{
+    public static class StartupSetup
+    {
+        public static async void AddSystemRolesToDb(this WebApplication app)
+        {
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<User>>();
+                    var roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    await SeedingRoles.SystemRoles(userManager, roleManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = loggerFactory.CreateLogger<Program>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+        }
+        public static void AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then " +
+                "your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                           Reference = new OpenApiReference
+                           {
+                               Type = ReferenceType.SecurityScheme,
+                               Id = "Bearer"
+                           }
+                        },
+                        Array.Empty<string>()
+
+                    }
+                });
+
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CoWorking.WebApi", Version = "v1" });
+            });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
+using LabWebApi.Web.Extensions;
+using LabWebAPI.Database.Data.Seeding;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,12 +11,20 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddRepositories();
 builder.Services.AddDbContext(builder.Configuration.GetConnectionString("DefaultConnection"));
 
+builder.Services.AddIdentityDbContext();
+
+//Custom swagger
+builder.Services.AddSwagger();
+
+
 builder.Services.AddSpaStaticFiles(configuration =>
 {
     configuration.RootPath = "ClientApp/dist";
 });
 
 var app = builder.Build();
+
+app.AddSystemRolesToDb();
 
 if (app.Environment.IsDevelopment())
 {
